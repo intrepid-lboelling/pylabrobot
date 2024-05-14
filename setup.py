e98feab434fb2479bb7fd4c1d4ef05d0b1f24663
intrepid-lboelling <lukas@intrepidlabs.ai>
2024-05-14 15:35:23 -0400

    Set include_package_data to True

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
   install_requires=["typing_extensions"],
   url="https://github.com/pylabrobot/pylabrobot.git",
   package_data={"pylabrobot": ["visualizer/*"]},
+  include_package_data=True,
   extras_require={
     "fw": extras_fw,
     "http": extras_http,
